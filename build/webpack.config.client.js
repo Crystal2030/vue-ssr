d43ac5e62b3492d622bd231bc34dbaec0761cc30
liuyun19 <shilei13@jd.com>
2018-10-22 18:26:58 +0800

    webpack4升级

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const ExtractPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HTMLPlugin = require('html-webpack-plugin');
 
 const baseConfig = require('./webpack.config.base');
@@ -31,7 +31,7 @@ const defaultPlugins = [
 
 if(isDev) {
     config = merge(baseConfig, {
-        devtool: '#cheanp-module-eval-source-map',
+        // devtool: '#cheanp-module-eval-source-map', // webpack4 自动加了这个配置
         module: {
             rules: [
                 {
@@ -53,7 +53,7 @@ if(isDev) {
         devServer,
         plugins: defaultPlugins.concat([
             new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoEmitOnErrorsPlugin(), // 减少不需要的一些信息展示问题
+            // new webpack.NoEmitOnErrorsPlugin(), // 减少不需要的一些信息展示问题  webpack4废弃了
         ])
     });
 
@@ -70,34 +70,53 @@ if(isDev) {
             rules: [
                 {
                     test: /\.styl/,
-                    use: ExtractPlugin.extract({
-                        fallback: 'vue-style-loader',
-                        use: [
-                            'css-loader',
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    sourceMap: true
-                                }
-                            },
-                            'stylus-loader'
-                        ]
-                    })
+                    use: [
+                      MiniCssExtractPlugin.loader,
+                      'css-loader',
+                      {
+
+                        loader:'postcss-loader',
+                        options: {
+                          sourceMap: true
+                        }
+
+                      },
+                      'stylus-loader'
+                    ]
+                    // use: ExtractPlugin.extract({
+                    //     fallback: 'vue-style-loader',
+                    //     use: [
+                    //         'css-loader',
+                    //         {
+                    //             loader: 'postcss-loader',
+                    //             options: {
+                    //                 sourceMap: true
+                    //             }
+                    //         },
+                    //         'stylus-loader'
+                    //     ]
+                    // })
                 }
             ],
         },
+       optimization: {  // webpack4 废弃了webpack.optimize.CommonsChunkPlugin
+         splitChunks: {
+           chunks: 'all'
+         },
+         runtimeChunk: true,
+       },
         plugins: defaultPlugins.concat([
-            new ExtractPlugin('styles.[contentHash:8].css'),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor'
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                // 注意： vendor要在runtime前面
-                // 把webpack生成在app.js里面webpack相关的代码单独打包到一个文件里面，
-                // 这样好处是，有新的模块加入的时候，webpack会给每个模块加一个id上去的，
-                // 插入的顺序可能在中间，导致后面的每个模块的id发生变化，发生变化后就会导致打包出来的内容的hash发生一定的变化，hash想要使用浏览器长缓存的目的就失效了
-                name: 'runtime'
-            })
+            new MiniCssExtractPlugin('styles.[contentHash:8].css'),
+            // new webpack.optimize.CommonsChunkPlugin({
+            //     name: 'vendor'
+            // }),
+            // new webpack.optimize.CommonsChunkPlugin({
+            //     // 注意： vendor要在runtime前面
+            //     // 把webpack生成在app.js里面webpack相关的代码单独打包到一个文件里面，
+            //     // 这样好处是，有新的模块加入的时候，webpack会给每个模块加一个id上去的，
+            //     // 插入的顺序可能在中间，导致后面的每个模块的id发生变化，发生变化后就会导致打包出来的内容的hash发生一定的变化，hash想要使用浏览器长缓存的目的就失效了
+            //     name: 'runtime'
+            // })
         ])
     });
 }
